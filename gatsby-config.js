fefef18985d82dd3575ffc3dccd3fd027af63309
Mathieu Gauthier <postmaster@mathieugauthier.fr>
2019-09-20 19:25:16 +0200

    Manifest logo

--- gatsby-config.js
+++ gatsby-config.js
@@ -244,19 +244,7 @@ module.exports = {
         display: `standalone`,
         orientation: `portrait`,
         include_favicon: false,
-        icon: `static/icons/manifest_logo.png`,
-        icons: [
-          {
-            src: `/icons/android-chrome-192x192.png`,
-            sizes: `192x192`,
-            type: `image/png`
-          },
-          {
-            src: `/icons/android-chrome-512x512.png`,
-            sizes: `512x512`,
-            type: `image/png`
-          }
-        ]
+        icon: `src/assets/manifest_logo.png`
       }
     },
     {
